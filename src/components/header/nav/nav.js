23249a2b4907bb6a74efa3d769acdc7a9fec09a6
Ryan Boucher <ryan.boucher@distributedlife.com>
2020-02-02 19:43:17 +1100

    Add presskit

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from './nav.css';
 import { SocialIcon } from 'react-social-icons';
+import { Link } from 'gatsby';
 
 const Nav = () => (
   <Container>
@@ -25,6 +26,16 @@ const Nav = () => (
           bgColor="white"
           fgColor="#222"
         />
+      </li>
+      <li>
+        <SocialIcon
+          url="https://instagram.com/seabrookstudios/"
+          bgColor="white"
+          fgColor="#222"
+        />
+      </li>
+      <li>
+        <Link to="/presskit/index.html">PRESS KIT</Link>
       </li>
     </ul>
   </Container>
---
+++ sync.sh
@@ -0,0 +1,2 @@
+aws s3 sync --profile ss-prod --delete --metadata-directive REPLACE --cache-control max-age=86400,public --acl public-read public s3://seabrook-studios.com
+# aws cloudfront create-invalidation --profile seabrook-studos-prod --distribution-id E1FDAP0GZNS44O --paths /index.html

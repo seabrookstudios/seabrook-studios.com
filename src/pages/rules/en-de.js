f7f83797acea66cbef64b7782531b3e43fa381a5
Ryan Boucher <ryan.boucher@distributedlife.com>
2020-09-13 18:46:55 +1000

    Translated rules for en-de, en-it and en-es

---
+++ src/pages/rules/en-de.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from 'components/layout';
+import Head from 'components/head';
+import MarkdownStyle from 'markdown.css.js';
+import { Columns } from '../../components/rule-styles';
+import Content from '../../components/content/en-de.js';
+
+const Rules = () => (
+  <Layout>
+    <Head pageTitle="en-de" />
+    <Columns className="markdown">
+      <MarkdownStyle />
+      <Content />
+    </Columns>
+  </Layout>
+);
+
+Rules.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Rules;

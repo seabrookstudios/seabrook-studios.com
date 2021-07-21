7cc99e3749b3b73ba419b42263a08bcd542bc9dd
Ryan Boucher <ryan.boucher@distributedlife.com>
2021-07-21 20:30:37 +1000

    Standard deployment sscript

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
-aws s3 sync --profile ss-prod --delete --metadata-directive REPLACE --cache-control max-age=86400,public --acl public-read public s3://seabrook-studios.com
+aws s3 sync --profile ss-prod --delete --metadata-directive REPLACE --cache-control max-age=86400,public --acl private public s3://seabrook-studios.com
 aws cloudfront create-invalidation --profile ss-prod --distribution-id E2YPNMIYXU1S8V --paths '/*'

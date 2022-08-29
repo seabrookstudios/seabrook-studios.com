b72d8c2dd2e8b0b1a193cff26e4b31d682dc232d
Ryan Boucher <ryan.boucher@distributedlife.com>
2022-08-29 12:12:09 +1000

    Add links to various policy pages

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -19,6 +19,38 @@ export const Footer = () => {
       {/* <PostList postList={latestPosts} title="Recent Posts" /> */}
       <img src={logo} alt="Seabrook Studios Logo" className="h-24 w-24 mt-4" />
       <p>© Seabrook Studios 2018-2022</p>
+      <ul>
+        <a className="px-1 underline" href="https://shop.seabrook-studios.com/pages/contact-us">
+          Contact Us
+        </a>
+        <a
+          className="px-1 underline"
+          href="https://shop.seabrook-studios.com/policies/privacy-policy"
+        >
+          Privacy Policy
+        </a>
+        <a
+          className="px-1 underline"
+          href="https://shop.seabrook-studios.com/policies/refund-policy"
+        >
+          Refunds
+        </a>
+        <a
+          className="px-1 underline"
+          href="https://shop.seabrook-studios.com/policies/shipping-policy"
+        >
+          Shipping
+        </a>
+        <a
+          className="px-1 underline"
+          href="https://shop.seabrook-studios.com/policies/terms-of-service"
+        >
+          Terms of Service
+        </a>
+        <a className="px-1 underline" href="https://shop.seabrook-studios.com/pages/ccpa-opt-out">
+          Do Not Sell My Data
+        </a>
+      </ul>
       <p className="text-xs text-gray-400">
         Apple logo is a trademark of Apple Inc. Google Play and the Google Play logo are trademarks
         of Google LLC.
